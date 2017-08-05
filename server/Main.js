26e6f3db0c831d934d16cfe71d10d239b236954d
chance <chance@Yotsugi.hsd1.ca.comcast.net>
2017-08-05 11:18:31 -0700

    Added basic skeleton code for the server

---
+++ server/Main.js
@@ -0,0 +1,83 @@
+var uuid = require('uuid')
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000, listen);
+
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+var io = require('socket.io')(server);
+
+setInterval(tick, 30);
+
+function tick() {
+    updateList = [entities, players];
+    io.sockets.emit('tick', updateList);
+}
+
+
+io.sockets.on('connection',
+    function(socket) {
+        console.log("Client Connected: " + socket.id);
+
+        // Create a new Player object associated with the socket ID
+        player = new Player(socket.id);
+        players.append(player);
+
+        entities.appdend(new Base(uuid.v4(), player, 100, 100));  // Spawn the player's base
+
+        // Set the player name
+        socket.on('name',
+            function(data) {
+                player.name = data;
+            })
+
+        // Spawn an Entity
+        socket.on('spawn',
+            function(data) {
+                // TODO: Check for ownership, distance, etc. and Spawn object
+            });
+
+        // Move Entities
+        socket.on('move',
+            function(data) {
+                // TODO: Check for ownership, movability, etc and set destination for data
+            });
+    });
+
+
+
+
+function Player(id) {
+    this.id = id;
+    this.name = '';
+}
+
+function Base(id, owner, x, y) {
+    this.id = id;
+    this.owner = owner;
+    this.x = x;
+    this.y = y;
+    this.maxHealth = 1000;
+    this.health = 1000;
+}
+
+
+function House(id, owner, x, y) {
+    this.id = id;
+    this.owner = owner;
+    this.x = x;
+    this.y = y;
+    this.maxHealth = 50;
+    this.health = 50;
+}
+
+
+var socket;
+var entities = [];
+var players = [];
